6813d94df6c7bede2f4920303520de934b06edcb
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-10-31 20:20:27 -0500

    styling too lazy for actual commit and future commits lol

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import { MdWeb, MdScreenSearchDesktop } from "react-icons/md";
 import { BsArrowUpRight } from "react-icons/bs";
 import { PiCodeBlock } from "react-icons/pi";
 import { TfiAnnouncement } from "react-icons/tfi";
+import Image from "next/image";
+import prestonHeadshot from "@/public/prestonHeadshot.webp";
+import daltonHeadshot from "@/public/daltonHeadshot.webp";
 
 export default function Home() {
 	return (
@@ -17,7 +20,7 @@ export default function Home() {
 				</div>
 			</section>
 			<section className="pt-10">
-				<h3 className="text-3xl">What We Do</h3>
+				<h3 className="text-4xl">What We Do</h3>
 				<div className="flex flex-col">
 					<div className="flex">
 						<MdWeb />
@@ -42,58 +45,83 @@ export default function Home() {
 				</div>
 			</section>
 			<section>
-				<h4>Why partner with us?</h4>
-				<div>
-					<div>
-						<span>Effective Communication</span>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Necessitatibus minima quos placeat rem nisi repudiandae animi,
-							reprehenderit saepe qui enim dolor, reiciendis itaque illo totam
-							natus eaque magni laboriosam illum. Ipsam quos facilis ea
-							distinctio!
-						</p>
+				<h4 className="pb-4 text-3xl font-thin">Why partner with us?</h4>
+				<div className="flex flex-row">
+					<div className="flex flex-col max-w-2xl">
+						<div>
+							<span className="text-2xl font-bold">
+								Effective Communication
+							</span>
+							<p>
+								We prioritize being responsive and quick with communication
+								because we value your time. You&apos;ll get a quick turnaround
+								from our team.
+							</p>
+						</div>
+						<div>
+							<span className="text-2xl font-bold">Technical Excellence</span>
+							<p>
+								We are up-to-date on the latest digital trends and leverage our
+								experience to provide technical excellence. Our expertise
+								delivers results.
+							</p>
+						</div>
 					</div>
-					<div>
-						<span>Technical Excellence</span>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Necessitatibus minima quos placeat rem nisi repudiandae animi,
-							reprehenderit saepe qui enim dolor, reiciendis itaque illo totam
-							natus eaque magni laboriosam illum. Ipsam quos facilis ea
-							distinctio!
-						</p>
-					</div>
-					<div>
-						<span>Focued on results</span>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Necessitatibus minima quos placeat rem nisi repudiandae animi,
-							reprehenderit saepe qui enim dolor, reiciendis itaque illo totam
-							natus eaque magni laboriosam illum. Ipsam quos facilis ea
-							distinctio!
-						</p>
-					</div>
-					<div>
-						<span>Locally owned and operated</span>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Necessitatibus minima quos placeat rem nisi repudiandae animi,
-							reprehenderit saepe qui enim dolor, reiciendis itaque illo totam
-							natus eaque magni laboriosam illum. Ipsam quos facilis ea
-							distinctio!
-						</p>
+					<div className="flex flex-col max-w-2xl">
+						<div>
+							<span className="text-2xl font-bold">Focued on results</span>
+							<p>
+								We drive real results so our partners can maximize profits and
+								see their investments pay off. We are focused on your success.
+							</p>
+						</div>
+						<div>
+							<span className="text-2xl font-bold">
+								Locally owned and operated
+							</span>
+							<p>
+								Based in Norman, Oklahoma, we understand how to help local
+								businesses thrive. Our team is invested in the community.
+							</p>
+						</div>
 					</div>
 				</div>
 			</section>
 			<section>
-				<h4>Recent Projects</h4>
+				<h4 className="pb-4 text-3xl font-thin">Recent Projects</h4>
 			</section>
 			<section>
-				<h4>Our Recent Reviews</h4>
+				<h4 className="pb-4 text-3xl font-thin">Our Recent Reviews</h4>
 			</section>
 			<section>
-				<h4>Contact Form</h4>
+				<h4 className="pb-4 text-3xl font-thin">Who We Are</h4>
+				<div>
+					<div>
+						<div>
+							<Image src={prestonHeadshot} alt="Preston Headshot" />
+						</div>
+						<div>
+							<span>Preston Jenkins</span>
+							<p>Owner & Developer</p>
+						</div>
+					</div>
+					<div>
+						<div>
+							<Image src={daltonHeadshot} alt="Preston Headshot" />
+						</div>
+						<div>
+							<span>Dalton Krueger</span>
+							<p>Owner & Operations</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section>
+				<h4 className="pb-4 text-3xl font-thin">
+					Need web-related sercives?
+					<br />
+					See how we can help
+				</h4>
 			</section>
 			<section>
 				<h4>Footer</h4>
